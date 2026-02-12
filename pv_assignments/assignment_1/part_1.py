64e2d1e0bcdf10cf574c02a3bf9353417010fe4c
Peter <s214236@dtu.dk>
2026-02-12 10:54:18 +0100

    Finished part 1-1

---
+++ pv_assignments/assignment_1/part_1.py
@@ -0,0 +1,84 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def load_data(am: float | int, water_vapor: int | None = None) -> pd.DataFrame:
+    file_name = f"Spectrum_AM{int(am)}"
+    if am != int(am):
+        file_name += f"_{str(am).split('.')[-1]}"
+    if water_vapor is not None:
+        file_name += f"_WP{water_vapor}"
+    folder_path = r"C:\Users\peter\Desktop\OneDrive - Danmarks Tekniske Universitet\Master\4. semester\34552 - Photovoltaic systems\PV - Gruppemappe\Assignment 1"
+    df = pd.read_excel(
+        f"{folder_path}\\{file_name}.xlsx", sheet_name="Spectral irradiance"
+    )
+    return df
+
+
+def plot(
+    dfs: list[pd.DataFrame],
+    labels: list[str],
+    value_names: list[str],
+    peak_wavelengths: list[float] | None = None,
+) -> None:
+    plt.figure()
+    for df, label in zip(dfs, labels):
+        for value_name in value_names:
+            plt.plot(
+                df["Wavelength (nm)"],
+                df[value_name],
+                label=label,  # TODO: fix labels for more columns
+            )
+
+    if peak_wavelengths is not None:
+        for x in peak_wavelengths:
+            plt.axvline(x, linestyle="--", color="gray", alpha=0.5)
+
+    plt.xlabel("Wavelength (nm)")
+    plt.ylabel("Global to perpendicular plane (W/m2/nm)")
+    plt.title("Spectral Irradiance for Different Air Masses")
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+
+def broadband_irradiance(dfs: list[pd.DataFrame], labels: list[str]) -> None:
+    for df, label in zip(dfs, labels):
+        irr_broadband = df["Global to perpendicular plane  (W/m2/nm)"].sum() * 10
+        print(f"Broadband Irradiance for {label}: {irr_broadband:.2f} W/m²")
+
+
+def peak_wavelength(dfs: list[pd.DataFrame], labels: list[str]) -> list[float]:
+    peak_irr = {}
+    for df, label in zip(dfs, labels):
+        max_irr = df["Global to perpendicular plane  (W/m2/nm)"].max()
+        peak_wavelength = df.loc[
+            df["Global to perpendicular plane  (W/m2/nm)"].idxmax(), "Wavelength (nm)"
+        ]
+        peak_irr[label] = (peak_wavelength, max_irr)
+    print("Peak Irradiance and Corresponding Wavelengths:")
+    for label, (wavelength, irradiance) in peak_irr.items():
+        print(
+            f"{label}: Wavelength = {wavelength} nm, Irradiance = {irradiance} W/m²/nm"
+        )
+
+    return [peak[0] for peak in peak_irr.values()]
+
+
+if __name__ == "__main__":
+    print("Part 1-1")
+    df_1_5 = load_data(1.5)
+    df_3 = load_data(3)
+    df_4_5 = load_data(4.5)
+    df_6 = load_data(6)
+    dfs = [df_1_5, df_3, df_4_5, df_6]
+    labels = ["AM 1.5", "AM 3", "AM 4.5", "AM 6"]
+    value_names = ["Global to perpendicular plane (W/m2/nm)"]
+    broadband_irradiance(dfs, labels)
+    peak_wavelengths = peak_wavelength(dfs, labels)
+    plot(dfs, labels, value_names, peak_wavelengths=peak_wavelengths)
+
+    print("\nPart 1-2")
+    df_1_5 = load_data(1.5)
+    dfs = [df_1_5]
+    labels = ["Global", "Direct", "Diffuse"]
